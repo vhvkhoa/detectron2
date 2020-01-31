f3848f422141099bf44922c6a999776f582752e9
Vo Ho Viet Khoa <vhvkhoa@Vos-MacBook-Pro.local>
2020-02-01 03:52:50 +0900

    update

--- tools/bbox_extract.py
+++ tools/bbox_extract.py
@@ -104,4 +104,4 @@ if __name__ == "__main__":
         if not os.path.isdir(args.output_dir):
             os.makedirs(args.output_dir)
         with open(os.path.join(args.output_dir, os.path.splitext(basename)[0] + '.json'), 'w') as f:
-            json.dump(video_instances, f)
+            json.dump(video_bboxes, f)
