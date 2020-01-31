09388160df0c68f7c7503d3d5850eef2133097ce
Vo Ho Viet Khoa <vhvkhoa@Vos-MacBook-Pro.local>
2020-02-01 03:33:13 +0900

    update

--- tools/bbox_extract.py
+++ tools/bbox_extract.py
@@ -10,10 +10,7 @@ import tqdm
 import json
 
 from detectron2.config import get_cfg
-from detectron2.data.detection_utils import read_image
 from detectron2.utils.logger import setup_logger
-
-from predictor import VisualizationDemo
 
 # constants
 WINDOW_NAME = "COCO detections"
