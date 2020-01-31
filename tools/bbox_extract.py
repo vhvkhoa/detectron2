40b49fab60d64257c98e20aeec5e90b575568507
Vo Ho Viet Khoa <vhvkhoa@Vos-MacBook-Pro.local>
2020-02-01 04:28:32 +0900

    update

--- tools/bbox_extract.py
+++ tools/bbox_extract.py
@@ -89,7 +89,7 @@ if __name__ == "__main__":
         for frame_preds in tqdm.tqdm(bbox_extractor.run_on_video(video), total=num_frames):
             frame_bboxes = []
             
-            boxes = frame_preds.pred_boxes.tolist() if frame_preds.has("pred_boxes") else None
+            boxes = frame_preds.pred_boxes.tensor.tolist() if frame_preds.has("pred_boxes") else None
             scores = frame_preds.scores.tolist() if frame_preds.has("scores") else None
             classes = frame_preds.pred_classes.tolist() if frame_preds.has("pred_classes") else None
 
