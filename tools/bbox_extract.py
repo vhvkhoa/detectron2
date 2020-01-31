a3b547c279dba3576446e9dfa61400012b444417
Vo Ho Viet Khoa <vhvkhoa@Vos-MacBook-Pro.local>
2020-02-01 03:55:55 +0900

    update

--- tools/bbox_extract.py
+++ tools/bbox_extract.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
 
     cfg = setup_cfg(args)
 
-    bbox_extractor = BboxExtractor(cfg, parallel=True)
+    bbox_extractor = BboxExtractor(cfg)
 
     for video_path in tqdm.tqdm(glob.glob(os.path.join(args.input_dir, '*'))):
         video = cv2.VideoCapture(video_path)
