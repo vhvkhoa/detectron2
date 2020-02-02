10e70f40f71793fda9376a3da1c52ea183808558
Vo Ho Viet Khoa <vhvkhoa@Vos-MacBook-Pro.local>
2020-02-03 03:00:27 +0900

    update

--- tools/bbox_extractor.py
+++ tools/bbox_extractor.py
@@ -64,7 +64,8 @@ class BboxExtractor(object):
             target_sampling_rate / 2,
             (num_frames + 1) * secs_per_frame - target_sampling_rate / 2,
             target_sampling_rate).tolist()
-        sampling_secs.append((target_sampling_rate * len(sampling_secs) + num_frames * secs_per_frame) / 2)
+        if target_sampling_rate * len(sampling_secs) < num_frames * secs_per_frame:
+            sampling_secs.append((target_sampling_rate * len(sampling_secs) + num_frames * secs_per_frame) / 2)
 
         frame_gen = self._frame_from_video(video)
         if self.parallel:
