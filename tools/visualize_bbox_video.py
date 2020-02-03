b7d2ad58f20e7e9ea783ec78dde6344090c930bc
Vo Ho Viet Khoa <vhvkhoa@Vos-MacBook-Pro.local>
2020-02-03 23:28:55 +0900

    update

--- tools/visualize_bbox_video.py
+++ tools/visualize_bbox_video.py
@@ -192,7 +192,7 @@ if __name__ == "__main__":
                 os.makedirs(os.path.dirname(video_output_path))
 
             video_output = cv2.VideoWriter(
-                filename=video_output_path,
+                filename=video_output_path + '.mkv',
                 # some installation of opencv may not support x264 (due to its license),
                 # you can try other format (e.g. MPEG)
                 fourcc=cv2.VideoWriter_fourcc(*"x264"),
