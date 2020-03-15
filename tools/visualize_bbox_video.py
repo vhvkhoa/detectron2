e012a03b6a54f7a58485e0d807e72aaea7d5d60b
Vo Ho Viet Khoa <vhvkhoa@Vos-MacBook-Pro.local>
2020-03-15 20:32:38 +0900

    update

--- tools/visualize_bbox_video.py
+++ tools/visualize_bbox_video.py
@@ -235,7 +235,9 @@ if __name__ == "__main__":
                             bbox['class_id'], bbox['box'],
                             mask_rle=None, color=None, ttl=8
                         )
-                        for bbox in video_bboxes[bbox_idx]['frame_bboxes'] if bbox['class_id'] in captured_ids
+                        for bbox in video_bboxes[bbox_idx]['frame_bboxes'] if (
+                            len(captured_ids) > 0 and bbox['class_id'] in captured_ids
+                            )
                     ]
                     colors, old_instances = _assign_colors(detected, old_instances)
 
